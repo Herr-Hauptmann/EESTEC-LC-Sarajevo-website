60479272fc6d6c43ff9ed0f1cf4782884d32c024
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-03-29 23:24:43 +0200

    Add project files.

---
+++ EESTEC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EESTEC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
